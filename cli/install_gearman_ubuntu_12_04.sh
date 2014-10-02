1ee3d114aaf38612f439f4bc7bfc6147991e39e7
Daniel Lamb <daniel@discoverygarden.ca>
2014-10-02 16:20:16 -0300

    Removing install script

--- cli/install_gearman_ubuntu_12_04.sh
+++
@@ -1,8 +0,0 @@
-apt-get install python-software-properties
-add-apt-repository ppa:gearman-developers/ppa
-apt-get update
-
-apt-get install libgearman-dev gearman-job-server gearman-tools
-pecl install gearman
-
-# Add extension=gearman.so to all applicable php.ini files.
