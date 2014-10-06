bb56e0ddf9d2dec609822391211c6a83e7e3370f
Daniel Lamb <daniel@discoverygarden.ca>
2014-10-06 17:04:03 -0300

    Router now responds to all jobs, not just the one.  Makes for better granularity for reporting or clearing out jobs of a certain type.

--- cli/islandora_worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -d "$1" ]; then
-  echo "Please provide the path to drupal root as an argument."
-  exit
-fi
-
-cd "$1"; gearman -v -w -f islandora_job xargs drush islandora-job-router &
